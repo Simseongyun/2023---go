a0a034f9807ef6ac969fc9ea258ff8b02783e277
seongyun3023@gmail.com <seongyun3023@gmail.com>
2023-09-25 14:41:40 +0900

    week05 v0.5

--- week05/test05_1.go
+++ week05/test05_1.go
@@ -9,6 +9,6 @@ import (
 func main() {
 	fmt.Print("Input score : ")
 	reader := bufio.NewReader(os.Stdin)
-	inputScore, _ := reader.ReadString('\n')
+	inputScore, _ := reader.ReadString('\n') // ignore the erro return value with the brank
 	fmt.Println(inputScore)
 }
